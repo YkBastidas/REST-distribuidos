d98e01fd92b6fb4997e8d29157123a8256925654
Joselyn Gonzalez <josygonzalez11@gmail.com>
2022-02-11 17:13:57 -0500

    Restaurar update

--- replicationCoordinator/replicationCoordinator.py
+++ replicationCoordinator/replicationCoordinator.py
@@ -22,11 +22,11 @@ class ReplicationCoordinator:
             with conn:
                 print("Connected by", addr)
                 while True:
-                    action_type = conn.recv(1024)  # RECEIVE REPLICATE/RESTORE
+                    action_type = conn.recv(1024)  # RECEIVE REPLICATE/RESTORE FROM API
                     print("Received", repr(action_type))
                     if action_type == 'REPLICATE':
                         conn.sendall(action_type)
-                        action = conn.recv(1024)  # RECEIVE COMMIT/ABORT
+                        action = conn.recv(1024)  # RECEIVE COMMIT/ABORT FROM API
                         print("Received", repr(action))
                         if not action:
                             break
@@ -40,19 +40,18 @@ class ReplicationCoordinator:
 
                             if action_type.decode("utf-8") == "REPLICATE":
                                 socket_replication.sendall(
-                                    action
-                                )  # SEND ACTION (COMMIT OR ABORT)
-                                action = socket_replication.recv(1024)  # RECEIVE COMMIT
+                                    action)  # SEND ACTION (COMMIT OR ABORT) 1
+                                action = socket_replication.recv(1024)  # RECEIVE COMMIT 2
                                 print("Received", repr(action))
-                                socket_replication.sendall(b"VOTE_REQUEST")
+                                socket_replication.sendall(b"VOTE_REQUEST")     #SEND VOTE_REQUEST 3
                                 vote = socket_replication.recv(
-                                    1024
-                                )  # RECEIVE VOTE_COMMIT OR VOTE_ABORT
+                                    1024)  # RECEIVE VOTE_COMMIT OR VOTE_ABORT 4
                                 print("Received", repr(vote))
                                 if vote.decode("utf-8") == "VOTE_COMMIT":
-                                    socket_replication.sendall(b"GLOBAL_COMMIT")
+                                    socket_replication.sendall(b"GLOBAL_COMMIT")    #SEND GLOBAL_COMMIT 5
                                     receive = socket_replication.recv(1024)
-                                    print("Received", repr(receive))  # RECEIVE GLOBAL
+                                    print("Received", repr(receive))  # RECEIVE GLOBAL 6
+                                    # EMPIEZA EL MANEJO DEL FILE
                                     root = os.path.dirname(__file__)
                                     relative_path = os.path.join(
                                         root,
@@ -62,34 +61,17 @@ class ReplicationCoordinator:
                                     )
                                     filename = os.path.realpath(relative_path)
                                     file = open(filename, "rb")
-                                    file_data = file.read(10240)
-                                    socket_replication.send(file_data)
+                                    file_data = file.read(10240)    # MANEJO DEL FILE
+                                    socket_replication.send(file_data)  # ENVIO DE DATA 7
                                     file.close()
                                     print("Done Sending!")
-                                    receive = socket_replication.recv(1024)
+                                    receive = socket_replication.recv(1024) #RECEIVE SUCCEED 8
                                 else:
-                                    socket_replication.sendall(b"GLOBAL_ABORT")
-                                    receive = socket_replication.recv(1024)
+                                    socket_replication.sendall(b"GLOBAL_ABORT") # SEND ABORT 9
+                                    receive = socket_replication.recv(1024) # RECEIVE FILED 10
                                 socket_replication.close()
                                 print("Received", repr(receive))  # RECEIVE REPLICATION OUTCOME
-                                conn.sendall(receive)  # SEND OUTCOME TO APP SERVER
-
-                            else:
-                                socket_replication.sendall(
-                                    action
-                                )  # SEND SERVER (A OR B)
-                                action = socket_replication.recv(1024)  # RECEIVE A O B
-                                print("Received", repr(action))
-                                if action.decode("utf-8") == "A":
-                                    socket_replication.sendall(action)
-                                    receive = socket_replication.recv(1024)
-                                    root = os.path.dirname(__file__)
-                                    filename = os.path.join(root, "aplicationServer" ,"objectsDatabase.json")
-                                    file = open(filename, "wb")
-                                    file_data = conn.recv(10240)
-                                    file.write(file_data)
-                                    file.close()
-                                    print("FILE RECEIVED")
+                                conn.sendall(receive)  # SEND OUTCOME TO APP SERVER (API)
 
                                 
 
@@ -103,18 +85,18 @@ class ReplicationCoordinator:
                             if action_type.decode("utf-8") == "REPLICATE":
                                 socket_replicationB.sendall(
                                     action
-                                )  # SEND ACTION (COMMIT OR ABORT)
-                                action = socket_replicationB.recv(1024)  # RECEIVE COMMIT
+                                )  # SEND ACTION (COMMIT OR ABORT) 1
+                                action = socket_replicationB.recv(1024)  # RECEIVE COMMIT 2
                                 print("Received", repr(action))
-                                socket_replicationB.sendall(b"VOTE_REQUEST")
+                                socket_replicationB.sendall(b"VOTE_REQUEST")    # 3
                                 vote = socket_replicationB.recv(
                                     1024
-                                )  # RECEIVE VOTE_COMMIT OR VOTE_ABORT
+                                )  # RECEIVE VOTE_COMMIT OR VOTE_ABORT 4
                                 print("Received", repr(vote))
                                 if vote.decode("utf-8") == "VOTE_COMMIT":
-                                    socket_replicationB.sendall(b"GLOBAL_COMMIT")
-                                    receive = socket_replicationB.recv(1024)
-                                    print("Received", repr(receive))  # RECEIVE GLOBAL
+                                    socket_replicationB.sendall(b"GLOBAL_COMMIT")   # 5
+                                    receive = socket_replicationB.recv(1024)    # RECEIVE GLOBAL 6
+                                    print("Received", repr(receive))  
                                     root = os.path.dirname(__file__)
                                     relative_path = os.path.join(
                                         root,
@@ -125,32 +107,61 @@ class ReplicationCoordinator:
                                     filename = os.path.realpath(relative_path)
                                     file = open(filename, "rb")
                                     file_data = file.read(10240)
-                                    socket_replicationB.send(file_data)
+                                    socket_replicationB.send(file_data) # SEND DATA 7
                                     file.close()
                                     print("Done Sending!")
-                                    receive = socket_replicationB.recv(1024)
+                                    receive = socket_replicationB.recv(1024)    # RECEIVE SUCCEED 8
                                 else:
-                                    socket_replicationB.sendall(b"GLOBAL_ABORT")
-                                    receive = socket_replicationB.recv(1024)
+                                    socket_replicationB.sendall(b"GLOBAL_ABORT") # SEND ABORT 9
+                                    receive = socket_replicationB.recv(1024)    # RECEIVE FAILED 10
                                 socket_replicationB.close()
                                 print("Received", repr(receive))  # RECEIVE REPLICATION OUTCOME
                                 conn.sendall(receive)  # SEND OUTCOME TO APP SERVER
-                            else:
-                                socket_replicationB.sendall(
-                                    action
-                                )  # SEND SERVER (A OR B)
-                                action = socket_replicationB.recv(1024)  # RECEIVE A O B
-                                print("Received", repr(action))
-                                if action.decode("utf-8") == "B":
-                                    socket_replicationB.sendall(action)
-                                    receive = socket_replicationB.recv(1024)
-                                    root = os.path.dirname(__file__)
-                                    filename = os.path.join(root, "aplicationServer" ,"objectsDatabase.json")
-                                    file = open(filename, "wb")
-                                    file_data = conn.recv(10240)
-                                    file.write(file_data)
-                                    file.close()
-                                    print("FILE RECEIVED")
+
+                    else:
+                        conn.sendall(action_type) # SEND TO THE API
+                        action = conn.recv(1024)  # RECEIVE A / B FROM API 
+                        print("Received", repr(action))
+                        if not action:
+                            break
+
+                        with socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM
+                        ) as socket_replication:
+                            socket_replication.connect(
+                                (HOST_REPLICATION_A, PORT_REPLICATION_A)
+                            )
+                            if action.decode("utf-8") == "A":
+                                socket_replication.sendall(action)  #SEND ACTION A 11
+                                receive = socket_replication.recv(1024)     #RECEIVE RESTAURAR 12
+                                print("Received", repr(receive))
+                                root = os.path.dirname(__file__)
+                                filename = os.path.join(root, "applicationServer" ,"objectsDatabaseRestore.json")
+                                file = open(filename, "wb")
+                                file_data = conn.recv(10240)    #RECEIVE DATA 13
+                                file.write(file_data)
+                                file.close()
+                                print("FILE RECEIVED")
+
+                        with socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM
+                        ) as socket_replicationB:
+                            socket_replicationB.connect(
+                                (HOST_REPLICATION_B, PORT_REPLICATION_B)
+                            )
+                            if action.decode("utf-8") == "B":
+                                socket_replication.sendall(action)  #SEND ACTION A 11
+                                receive = socket_replication.recv(1024)     #RECEIVE RESTAURAR 12
+                                print("Received", repr(receive))
+                                root = os.path.dirname(__file__)
+                                filename = os.path.join(root, "applicationServer" ,"objectsDatabaseRestore.json")
+                                file = open(filename, "wb")
+                                file_data = conn.recv(10240)    #RECEIVE DATA 13
+                                file.write(file_data)
+                                file.close()
+                                print("FILE RECEIVED")
+
+
 
 
     def restoreObjects():
--- replicationServer/replicationServerA.py
+++ replicationServer/replicationServerA.py
@@ -15,42 +15,49 @@ class ReplicationServer:
             with conn:
                 print("Connected by", addr)
                 while True:
-                    action = conn.recv(1024)  # RECEIVE ACTION
+                    action = conn.recv(1024)  # RECEIVE ACTION 1
                     print("Received", repr(action))
                     if(action.decode("UTF-8") == "COMMIT"):
-                        conn.sendall(action)  # SEND ACTION
-                        vote_request = conn.recv(1024)  # RECEIVE VOTE_REQUEST
+                        conn.sendall(action)  # SEND ACTION 2
+                        vote_request = conn.recv(1024)  # RECEIVE VOTE_REQUEST 3
                         print("Received", repr(vote_request))
                         action = action.decode("utf-8")
                         if not action:
                             break
                         else:
                             if action == "COMMIT":
-                                conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT
+                                conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT 4
                             elif action == "ABORT":
-                                conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT
+                                conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT 4
                             else:
                                 choose = choice(["COMMIT", "ABORT"])
                                 if choose == "COMMIT":
-                                    conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT
+                                    conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT 4
                                 else:
-                                    conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT
+                                    conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT 4
 
-                        last = conn.recv(1024)  # RECEIVE GLOBAL
+                        last = conn.recv(1024)  # RECEIVE GLOBAL 5
                         print("Received", repr(last))
                         if last.decode("utf-8") == "GLOBAL_COMMIT":
-                            conn.sendall(last)
+                            conn.sendall(last)  # 6
+                            # CREA EL FILE
                             root = os.path.dirname(__file__)
                             filename = os.path.join(root, "replicationDatabase.json")
                             file = open(filename, "wb")
-                            file_data = conn.recv(10240)
+                            file_data = conn.recv(10240) # RECIBE LA DATA 7
+                            # LLENA EL ARCHIVO
                             file.write(file_data)
                             file.close()
                             print("FILE RECEIVED")
-                            conn.sendall(b"SUCCEED REPLICATION")
+                            conn.sendall(b"SUCCEED REPLICATION")    # SEND SUCCEED 8
                         else:
-                            conn.sendall(b"FAILED REPLICATION")
+                            t = conn.recv(1024)     # RECEIVE ABORT 9
+                            conn.sendall(b"FAILED REPLICATION") # SEND FAILED 10
+                            print("Received", repr(t)) 
                     else:
+                        serv = conn.recv(1024)  # RECEIVE A 11
+                        conn.sendall(b"RESTAURAR")  #SEND RESTAURAR 12
+                        print("Received", repr(serv))
                         root = os.path.dirname(__file__)
                         relative_path = os.path.join(
                             root,
@@ -61,7 +68,7 @@ class ReplicationServer:
                         filename = os.path.realpath(relative_path)
                         file = open(filename, "rb")
                         file_data = file.read(10240)
-                        conn.send(file_data)
+                        conn.send(file_data) # SEND DATA 13
                         file.close()
                         print("Done Sending!")
                         
