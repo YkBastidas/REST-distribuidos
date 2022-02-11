ebec5d1a2da1d98cdfa737b435821fd9389b3839
Yorfrank Bastidas <44125021+YkBastidas@users.noreply.github.com>
2022-02-11 02:33:44 -0400

    Arreglando errores de replication (incompleto)

--- replicationServer/replicationServerA.py
+++ replicationServer/replicationServerA.py
@@ -14,32 +14,34 @@ class ReplicationServer:
             with conn:
                 print("Connected by", addr)
                 while True:
-                    #data = conn.recv(1024)
-                    #print("Received", repr(data))
-                    #if not data:
-                        #break
-                    #conn.sendall(data)
-
-                    
-                    data = conn.recv(1024)  # RECEIVE ACTION
-                    print("Received", repr(data))
-                    if not data:
+                    # data = conn.recv(1024)
+                    # print("Received", repr(data))
+                    # if not data:
+                    # break
+                    # conn.sendall(data)
+                    action = conn.recv(1024)  # RECEIVE ACTION
+                    print("Received", repr(action))
+                    conn.sendall(action)  # SEND ACTION
+                    vote_request = conn.recv(1024)  # RECEIVE VOTE_REQUEST
+                    print("Received", repr(vote_request))
+                    action = action.decode("utf-8")
+                    if not action:
                         break
                     else:
-                        if data == 'COMMIT':
-                            conn.sendall(b"VOTE_COMMIT")    # SEND VOTE_COMMIT
-                        if data == 'ABORT':
-                            conn.sendall(b"VOTE_ABORT")    # SEND VOTE_ABORT
+                        if action == "COMMIT":
+                            conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT
+                        if action == "ABORT":
+                            conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT
                         else:
-                            choose = choice(['COMMIT', 'ABORT'])
-                            if choose == 'COMMIT':
-                                conn.sendall(b"VOTE_COMMIT")    # SEND VOTE_COMMIT
+                            choose = choice(["COMMIT", "ABORT"])
+                            if choose == "COMMIT":
+                                conn.sendall(b"VOTE_COMMIT")  # SEND VOTE_COMMIT
                             else:
-                                conn.sendall(b"VOTE_ABORT")    # SEND VOTE_ABORT
-                    
+                                conn.sendall(b"VOTE_ABORT")  # SEND VOTE_ABORT
+
                     last = conn.recv(1024)  # RECEIVE GLOBAL
                     print("Received", repr(last))
-                    if last == 'GLOBAL_COMMIT':
+                    if last.decode("utf-8") == "GLOBAL_COMMIT":
                         conn.sendall(b"SUCCEED REPLICATION")
                     else:
                         conn.sendall(b"FAILED REPLICATION")
